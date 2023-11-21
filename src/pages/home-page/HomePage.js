b9fe8e4ff0dea317611bc4860a4de9d81fe935f6
George Ntogramatzis <geontog@gmail.com>
2023-11-21 16:40:52 +0000

    changes to screen size

--- src/pages/home-page/HomePage.js
+++ src/pages/home-page/HomePage.js
@@ -28,7 +28,7 @@ function HomePage() {
   React.useEffect(() => {
     setIsMobile(
       width <= 768 ||
-        (width > 768 && height <= 500) ||
+        (width > 768 && height <= 505) ||
         (height <= 767 && width <= 768)
     );
   }, [width, height]);
