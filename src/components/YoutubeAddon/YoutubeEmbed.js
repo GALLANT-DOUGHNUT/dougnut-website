d5ae5c90923b2bc9f57e82446996d414efd6e090
Cris Hasan <71494397+CrisHasan@users.noreply.github.com>
2026-04-28 12:52:33 +0100

    Revert "Merge Jonathan patches into Cris branch "

--- src/components/YoutubeAddon/YoutubeEmbed.js
+++ src/components/YoutubeAddon/YoutubeEmbed.js
@@ -1,30 +1,21 @@
-import React from "react";
+import React from 'react'
 import PropTypes from "prop-types";
 
-const YoutubeEmbed = ({ width, embedId }) => (
-  <div
-    className="video-responsive"
-    style={{
-      width: width <= 768 ? "100%" : "55%",
-      position: "relative",
-      left: width <= 768 ? "0%" : "22%",
-      margin: "32px 0",
-    }}
-  >
+const YoutubeEmbed = ({ embedId }) => (
+  <div className="video-responsive" style={{position: "relative", width:"100%"}}>
     <iframe
-      style={{ borderRadius: 4, width: "100%", aspectRatio: "16/9" }}
-      src="https://www.youtube.com/embed/xCVGq8z-UWY?si=J3wX3Wv87k4KEhjI"
-      title="YouTube video player"
-      frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      allowfullscreen
+      style={{width:"100%", "aspectRatio": "16/9"}}
+      src={`https://www.youtube.com/embed/${embedId}`}
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
-    ></iframe>
+      title="Embedded youtube"
+    />
   </div>
 );
 
 YoutubeEmbed.propTypes = {
-  embedId: PropTypes.string.isRequired,
+  embedId: PropTypes.string.isRequired
 };
 
 export default YoutubeEmbed;
